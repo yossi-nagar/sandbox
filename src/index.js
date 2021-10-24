3c9c539f8241ad3caba838138a06675f90047f88
Yossi Nagar <yossin@cliotech.com>
2021-10-24 12:27:33 +0300

    accordion

--- src/index.js
+++ src/index.js
@@ -1,35 +1,23 @@
 import './style.css';
 
-// let accordion = document.querySelector('.accordion'),
-//     title = accordion.querySelector('.title'),
-//     content = accordion.querySelector('.content');
-
-
-// function slideDown(timestamp) {
-//     let accordionHeight = accordion.getBoundingClientRect().height;
-//     let contentHeight = content.getBoundingClientRect().height;
-//     console.log(accordionHeight, contentHeight)
-//     accordion.style.maxHeight = `${accordionHeight+contentHeight}px`;
-// }
-// title.addEventListener('click', e => {
-    
-//     window.requestAnimationFrame(slideDown);
-// });
-
 document.querySelectorAll('.accordion').forEach(accordion => {
     let title = accordion.querySelector('.title'),
     content = accordion.querySelector('.content'),
     initialHeight = accordion.style.maxHeight;
     title.addEventListener('click', e => {
-        if(!accordion.classList.contains('active')) {
-            accordion.classList.add('active');
+        if(!title.classList.contains('active')) {
+           
             window.requestAnimationFrame(ts => {
+                title.classList.add('active');
+                title.classList.remove('not-active');
                 let maxHeight = accordion.getBoundingClientRect().height + content.getBoundingClientRect().height;
                 accordion.style.maxHeight = `${maxHeight}px`;
             });
         } else {
-            accordion.classList.remove('active');
+            
             window.requestAnimationFrame(ts => {
+                title.classList.add('not-active');
+                title.classList.remove('active');
                 accordion.style.maxHeight = initialHeight;
             });
         }
